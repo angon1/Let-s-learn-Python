ba386a699fdea8f2d86be9038fe3fef7e95febaa
angon1 <kubabak@gmail.com>
2020-05-26 16:31:31 +0200

    First class

---
+++ pullUps/training.py
@@ -0,0 +1,43 @@
+class Excercise:
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def setName(name):
+        self.name = name
+
+    def setDescription(description):
+        self.description = description
+
+    def getName(self):
+        return self.name
+
+    def getDescription(self):
+        return self.description
+
+class SingleSet:
+
+    
+    def __init__(self, repsNumber, breakTime):
+        self.repsNumber = repsNumber
+        self.breakTime = breakTime
+        excercise = []
+
+    def addExcercise(self):
+        print("")
+
+
+class ExcerciseBlock:
+    blockName = ""
+    numOfExcercises = 0
+    blockReps = 0
+
+class Training:
+    name = "Training name"
+    blocks = []
+
+
+
+
+
