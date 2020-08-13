0eca0c53a277c51d2e761e5d2f63026e7f896bf6
angon1 <kubabak@gmail.com>
2020-08-13 11:44:04 +0200

    db

--- pullUpsWeb/settings.py
+++ pullUpsWeb/settings.py
@@ -91,7 +91,6 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 DATABASE_URL = os.environ['DATABASE_URL']
-
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # Password validation
