217f2e95b22bffd44c91cc2a780d85e21c08263c
angon1 <kubabak@gmail.com>
2020-08-13 11:14:53 +0200

    dzialajacy css

--- pullUpsWeb/settings.py
+++ pullUpsWeb/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = 'pullUps/static/'
+STATIC_URL = '/pullUps/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'pullUps/static/')
 
 # STATICFILES_DIRS = (
