14d49af6e2c2a2dacee9d6e051d06ad5a4fa50fb
angon1 <kubabak@gmail.com>
2020-08-13 12:02:02 +0200

    django heroku

--- pullUpsWeb/settings.py
+++ pullUpsWeb/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import psycopg2
+import django_heroku
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
